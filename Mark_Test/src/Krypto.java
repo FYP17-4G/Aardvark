d6d046925f3fc639dccc7cd2c687be59afd57834
Mark Christain <klass_mark@hotmail.com>
2017-12-13 17:16:41 +0800

    Fixed FrequencyAnalysis
    FrequencyAnalysis (f <seqLen> <n>) is completed.

--- Mark_Test/src/Krypto.java
+++ Mark_Test/src/Krypto.java
@@ -6,8 +6,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 
 public class Krypto {
     private static final Character[] OPTIONS = {'f', 'g','i', 'l', 'p', 'q', 's', 'S', 'T', 't', 'B', 'b', 'u', 'z', 'r', 'w'};
@@ -18,6 +17,7 @@ public class Krypto {
         String[] params;
 
         ORIGINAL_DATA = readFile("plain.txt");
+        MODIFIED_DATA = ORIGINAL_DATA;
         do {
             response = displayMenu();
             params = response.split(" ");
@@ -25,11 +25,18 @@ public class Krypto {
             switch (params[0]) {
                 case "i": {
                     indexOfCoincidence(params);
+                    break;
+                }
+
+                case "f": {
+                    frequencyAnalysis(params);
+                    break;
                 }
             }
         } while (!response.equals ("q"));
     }
 
+    //print the required output for `i <n>`
     private static void indexOfCoincidence (String[] params) {
         int period;
         try {
@@ -39,7 +46,7 @@ public class Krypto {
             period = 1;
         }
 
-        ArrayList<Double> ICs = getIC(period, ORIGINAL_DATA);
+        ArrayList<Double> ICs = getIC(period, MODIFIED_DATA);
 
         int counter = 1;
         for (Double ic: ICs) {
@@ -48,6 +55,92 @@ public class Krypto {
             else
                 System.out.println("IC " + (counter++) + " = " + ic);
         }
+    }
+
+    //print the required output for `f <seqLen> <n>`
+    private static void frequencyAnalysis (String[] params) {
+        //commonly you'll search for trigrams, so default is 3.
+        Integer sequenceLength = 3, maxEntries = 0;
+        String data = MODIFIED_DATA.toLowerCase().replaceAll("[^A-Za-z]", "");
+
+        if (params.length > 1) {
+            try {
+                sequenceLength = Integer.parseInt(params[1]);
+
+                //if maxEntries exists
+                if (params.length > 2) {
+                    try {
+                        maxEntries = Integer.parseInt(params[2]);
+                    } catch (NumberFormatException _maxEntries_) {
+                        //if 0, then show all
+                        maxEntries = 0;
+                    }
+                }
+            } catch (NumberFormatException _sequenceLength_) {
+                sequenceLength = 3;
+            }
+        }
+
+        String checkSequence, currentSequence;
+        Set<String> sequenceSet = new HashSet<>();
+
+        //find all possible sequences
+        for (int i = 0; i < data.length(); ++i) {
+            //make sure it doesn't go out of bounds.
+            if (i + sequenceLength >= data.length())
+                break;
+
+            checkSequence = data.substring(i, i + sequenceLength);
+            sequenceSet.add(checkSequence);
+        }
+
+        List<String> sequences = new ArrayList<>(sequenceSet);
+        Integer[] seqCounter = new Integer[sequences.size()];
+        Arrays.fill(seqCounter, 0);
+
+        for (int j = 0; j < data.length(); ++j) {
+            if (j + sequenceLength >= data.length())
+                break;
+
+            currentSequence = data.substring(j, j + sequenceLength);
+
+            if (sequences.contains(currentSequence)) {
+                int index = sequences.indexOf(currentSequence);
+                ++seqCounter[index];
+            }
+        }
+
+        ArrayList<Integer> sequenceCounter = new ArrayList<>(Arrays.asList(seqCounter));
+
+        if (maxEntries == 0 || maxEntries > sequenceCounter.size()) {
+            maxEntries = sequenceCounter.size();
+        }
+
+        for (int i = 0; i < maxEntries; ++i) {
+            int highest = getHighestIndex(sequenceCounter);
+            String output = sequences.get(highest) + ": " + sequenceCounter.get(highest);
+
+            sequenceCounter.remove(highest);
+            sequences.remove(highest);
+
+            System.out.println(output);
+        }
+    }
+
+    private static Integer getHighestIndex (ArrayList<Integer> input) {
+        int maxVal = 0, maxInd = 0;
+
+        int counter = 0;
+        for (Integer in: input) {
+            if (maxVal < in) {
+                maxVal = in;
+                maxInd = counter;
+            }
+
+            ++counter;
+        }
+
+        return maxInd;
     }
 
     //get IC for a bunch of data, reading from every nth letter.
