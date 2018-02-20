d16b818293353f0d63940c2a03ca629267938d2a
Mark Christian <klass_mark@hotmail.com>
2018-02-20 18:22:25 +0800

    Added Name and Description for Shift

--- Krypto_4_Java/src/main/java/models/ciphers/Shift.java
+++ Krypto_4_Java/src/main/java/models/ciphers/Shift.java
@@ -9,123 +9,131 @@
  *
  * If no key is provided, the program will assume a key of '3', which is, essentially, a Caesar Cipher.
  * ---------
-*/
+ */
 
 package models.ciphers;
 
 public class Shift extends AbstractCipher implements CipherInterface {
-    private final String NAME = "Shift Cipher";
-  private final String DESC = "Shifts the input by 'key'. Allows encryption & decryption.";
+  private final String NAME = "Shift Cipher";
+  private static final String DESC = "A common example of the Shift Cipher is the CAESAR CIPHER. It works by shifting" +
+    " the characters <x> characters to the right to encrypt it, and <x> characters to the left to decrypt." + "\n" +
+    "EXAMPLE" + "\n" +
+    "-------" + "\n" +
+    "KEY: 3" + "\n" +
+    "PT:  THIS IS THE PLAINTEXT" + "\n" +
+    "CT:  WKLV LV WKH SODLQWHAW";
 
 //    //so it can't be instantiated
 //    private Shift(){}
 
-    //caesar cipher by default
-    public String encrypt (String plaintext) {
-        StringBuilder sb = new StringBuilder();
+  //caesar cipher by default
+  public String encrypt(String plaintext) {
+    StringBuilder sb = new StringBuilder();
 
-        Integer offset = 3;
-        int output;
-        for (Character c: plaintext.toCharArray()) {
-            output = c - 'a' + offset;
-            output %= 26;
+    Integer offset = 3;
+    int output;
+    for (Character c : plaintext.toCharArray()) {
+      output = c - 'a' + offset;
+      output %= 26;
 
-            sb.append(ALPHABETS.charAt(output));
-        }
-
-        return sb.toString();
+      sb.append(ALPHABETS.charAt(output));
     }
 
-    public String decrypt (String ciphertext) {
-        StringBuilder sb = new StringBuilder();
+    return sb.toString();
+  }
 
-        Integer offset = 3;
-        int output;
+  public String decrypt(String ciphertext) {
+    StringBuilder sb = new StringBuilder();
 
-        for (Character c: ciphertext.toCharArray()) {
-            output = c - 'a' - offset;
+    Integer offset = 3;
+    int output;
 
-            if (output < 0) {
-                output += 26;
-            }
-            output %= 26;
+    for (Character c : ciphertext.toCharArray()) {
+      output = c - 'a' - offset;
 
-            sb.append(ALPHABETS.charAt(output));
-        }
+      if (output < 0) {
+        output += 26;
+      }
+      output %= 26;
 
-        return sb.toString();
+      sb.append(ALPHABETS.charAt(output));
     }
 
-    @Override
-    public String encrypt (String plaintext, String key)  {
-      key = key.trim();
+    return sb.toString();
+  }
 
-      if (checkKey(key)) {
-        StringBuilder sb = new StringBuilder();
+  @Override
+  public String encrypt(String plaintext, String key) {
+    key = key.trim();
 
-        Integer offset = Integer.parseInt(key);
-        int output;
-        for (Character c : plaintext.toCharArray()) {
-          output = c - 'a' + offset;
-          output %= 26;
+    if (checkKey(key)) {
+      StringBuilder sb = new StringBuilder();
 
-          sb.append(ALPHABETS.charAt(output));
+      Integer offset = Integer.parseInt(key);
+      int output;
+      for (Character c : plaintext.toCharArray()) {
+        output = c - 'a' + offset;
+        output %= 26;
+
+        sb.append(ALPHABETS.charAt(output));
+      }
+      return sb.toString();
+    }
+
+    return "failed.";
+  }
+
+  @Override
+  public String decrypt(String ciphertext, String key) {
+    key = key.trim();
+    if (checkKey(key)) {
+      StringBuilder sb = new StringBuilder();
+
+      Integer offset = Integer.parseInt(key);
+      int output;
+
+      for (Character c : ciphertext.toCharArray()) {
+        output = c - 'a' - offset;
+
+        if (output < 0) {
+          output += 26;
         }
-        return sb.toString();
+        output %= 26;
+
+        sb.append(ALPHABETS.charAt(output));
       }
 
-      return "failed.";
+      return sb.toString();
     }
 
-    @Override
-    public String decrypt (String ciphertext, String key)  {
-      key = key.trim();
-      if (checkKey(key)) {
-        StringBuilder sb = new StringBuilder();
-
-        Integer offset = Integer.parseInt(key);
-        int output;
-
-        for (Character c : ciphertext.toCharArray()) {
-          output = c - 'a' - offset;
-
-          if (output < 0) {
-            output += 26;
-          }
-          output %= 26;
-
-          sb.append(ALPHABETS.charAt(output));
-        }
-
-        return sb.toString();
-      }
-
-      return "failed.";
+    return "failed.";
 
 //      key = String.valueOf(Integer.parseInt(key) * (-1));
 //      return encrypt(ciphertext, key);
+  }
+
+  @Override
+  public String getDescription() {
+    return DESC;
+  }
+
+  @Override
+  public String getName() {
+    return NAME;
+  }
+
+  /**
+   * Checks if the string can be converted to an integer.
+   */
+  @Override
+  public Boolean checkKey(String key) {
+
+    try {
+      Integer.parseInt(key);
+    } catch (NumberFormatException nfe) {
+      return false;
     }
 
-    @Override
-    public String getDescription() { return cipherDescription; }
-
-    @Override
-    public String getName() {
-        return cipherName;
-    }
-
-    /**
-     * Checks if the string can be converted to an integer.
-     */
-    @Override
-    public Boolean checkKey(String key)  {
-
-        try {
-            Integer.parseInt(key);
-        } catch (NumberFormatException nfe) {
-            return false;
-        }
-
-        return true;
-    }
+    return true;
+  }
 }
